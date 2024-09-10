8ce49c4543fe1d7457ed41a8ab00777c0000444c
im <imartemy1524@gmail.com>
2024-09-10 16:33:20 +0200

    Add global notifications about objects to websocket

--- websocket.ts
+++ websocket.ts
@@ -1,26 +1,25 @@
-import {INotificationJson, IOneMessageJson} from "./database";
+import {IBloggerJson, INotificationJson, IOneAnimeJson, IOneMessageJson} from "./database";
 
-export type ClientEventTypes = 'message-read'|'new-message'|'notification'|'error'|'statistics' | 'message-deleted' | 'message-edited';
-export type ServerEventTypes = 'read-messages'|'chat-subscribe'|'subscribe-events'|'unsubscribe-events';
+export type SubscribableObjectType = 'blogger' | 'anime';
 
-//client
-export interface ClientEvent{
-    event: ClientEventTypes;
-    [k: string]: unknown;
+interface ClientEvent{
+    event: string;
 }
-export interface ErrorEvent extends ClientEvent{
+//client
+export interface ErrorEvent extends ClientEvent {
     event: 'error';
     error: string;
     error_code?: number;
     error_name?: string;
 }
-export interface StatisticsUpdateEvent extends ClientEvent{
+
+export interface StatisticsUpdateEvent extends ClientEvent {
     event: 'statistics';
-    users: {count: number; online: number;};
-    animes: {count: number};
-    comments: {count: number};
-    reviews: {count: number};
-    tabs: {count: number};
+    users: { count: number; online: number; };
+    animes: { count: number };
+    comments: { count: number };
+    reviews: { count: number };
+    tabs: { count: number };
     episodes: {
         h1: number;
         h3: number;
@@ -29,47 +28,107 @@ export interface StatisticsUpdateEvent extends ClientEvent{
     }
 }
 
-export interface NewNotificationEvent extends ClientEvent{
+export interface NewNotificationEvent extends ClientEvent {
     event: 'notification'
     message: INotificationJson
 }
-export interface NewMessageEvent extends ClientEvent{
+
+export interface NewMessageEvent extends ClientEvent {
     message: IOneMessageJson;
     event: 'new-message'
 }
-export interface MessageReadEvent extends ClientEvent{
+
+export interface MessageReadEvent extends ClientEvent {
     event: 'message-read';
     user_id: number;
     is_owner: boolean;
 }
 
-export interface MessageEditedEvent extends ClientEvent{
+export interface MessageEditedEvent extends ClientEvent {
     event: 'message-edited',
     message: IOneMessageJson
 }
 
-export interface MessageDeletedEvent extends ClientEvent{
+export interface MessageDeletedEvent extends ClientEvent {
     event: 'message-deleted',
     message: IOneMessageJson
 }
+export interface UpdateBloggerEvent extends ClientEvent {
+    event: 'update-blogger';
+    blogger_id: number;
+    data: Partial<IBloggerJson>;
+}
+export interface UpdateAnimeEvent extends ClientEvent {
+    event: 'update-anime';
+    anime_id: number;
+    data: Partial<IOneAnimeJson>;
+}
 
+export type AllClientEvents = {
+    'error': ErrorEvent;
+    'statistics': StatisticsUpdateEvent;
+    'notification': NewNotificationEvent;
+    'new-message': NewMessageEvent;
+    'message-read': MessageReadEvent;
+    'message-edited': MessageEditedEvent;
+    'message-deleted': MessageDeletedEvent;
+    'update-blogger':UpdateBloggerEvent;
+    'update-anime': UpdateAnimeEvent;
+    // Add other mappings as needed
+};
+export type ClientEventTypes = keyof AllClientEvents;
 
 //server
 
-export interface WebSocketEvent{
-    event: ServerEventTypes;
-}
-export interface SendMessage{
+
+export interface SendMessage {
     message: string;
     to_user: number;
     answer_to: number | null;
 }
 
-export interface ReadMessage extends WebSocketEvent{
+export interface ReadMessage {
     event: 'read-messages';
     user_id: number;
 }
-export interface SubscribeForChatMessages extends WebSocketEvent{
+
+export interface SubscribeForChatMessages {
     event: 'chat-subscribe';
     subscribe: boolean;
 }
+
+export interface SubscribeGlobal {
+    event: 'subscribe-global';
+    /**
+     * Object type to subscribe to
+     */
+    objectType: SubscribableObjectType;
+    /**
+     * Object id to subscribe to
+     */
+    objectId: number | string;
+    /**
+     * Unique key for this subscription
+     */
+    key: string;
+}
+
+export interface UnsubscribeGlobal {
+    event: 'unsubscribe-global';
+    objectType: SubscribableObjectType;
+    objectId: number | string;
+    key: string;
+}
+
+export type WebSocketEvents = {
+    'subscribe-global': SubscribeGlobal;
+    'unsubscribe-global': UnsubscribeGlobal;
+    'chat-subscribe': SubscribeForChatMessages;
+    'read-messages': ReadMessage;
+    'subscribe-events': { event: 'subscribe-events' };
+    'unsubscribe-events': { event: 'unsubscribe-events' };
+    // Add other mappings as needed
+};
+
+export type ServerEventTypes = keyof WebSocketEvents;
+
