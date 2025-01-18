ad8f6ab32c0b2561f28bc988bdb6126e85a693eb
im <imartemy1524@gmail.com>
2025-01-18 18:14:40 +0200

    Add screenshots

---
+++ structs.js
@@ -0,0 +1,10 @@
+﻿export var MessageStatusComplaints;
+(function (MessageStatusComplaints) {
+    MessageStatusComplaints[MessageStatusComplaints["Add"] = 0] = "Add";
+    MessageStatusComplaints[MessageStatusComplaints["Reject"] = 1] = "Reject";
+    MessageStatusComplaints[MessageStatusComplaints["Approved"] = 2] = "Approved";
+    MessageStatusComplaints[MessageStatusComplaints["Resotred"] = 3] = "Resotred";
+    MessageStatusComplaints[MessageStatusComplaints["Argue"] = 4] = "Argue";
+})(MessageStatusComplaints || (MessageStatusComplaints = {}));
+;
+//# sourceMappingURL=structs.js.map
